365ed33d6027ab463afc8e50c4da8190293f593a
aphro_kuma <751923779@qq.com>
2025-06-21 14:49:20 +0800

    code inital

---
+++ dynamicHot/src/test/java/com/kuma/tools/dynamicHot/DynamicHotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuma.tools.dynamicHot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicHotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
