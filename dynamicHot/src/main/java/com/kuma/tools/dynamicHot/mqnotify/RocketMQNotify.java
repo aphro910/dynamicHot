b730d9af3b6a419f899e9d7b5d43ff3b3f305547
aphro_kuma <751923779@qq.com>
2025-06-23 16:43:23 +0800

    update

--- dynamicHot/src/main/java/com/kuma/tools/dynamicHot/mqnotify/RocketMQNotify.java
+++ dynamicHot/src/main/java/com/kuma/tools/dynamicHot/mqnotify/RocketMQNotify.java
@@ -20,24 +20,23 @@ public class RocketMQNotify implements MQNotify{
 
     @Override
     public void report(Map<String, Integer> map) {
-        System.out.println("rocketmq reporting");
-//        try {
-//            byte[] compress = CompressUtil.compress(JSONUtil.toJsonStr(map));
-//            rocketMQTemplate.asyncSend("ROCKET_MQ_HOT_KEY_ANALYSIS",compress, new SendCallback() {
-//
-//                @Override
-//                public void onSuccess(SendResult sendResult) {
-//
-//                }
-//
-//                @Override
-//                public void onException(Throwable throwable) {
-//
-//                }
-//            });
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        try {
+            byte[] compress = CompressUtil.compress(JSONUtil.toJsonStr(map));
+            rocketMQTemplate.asyncSend("ROCKET_MQ_HOT_KEY_ANALYSIS",compress, new SendCallback() {
+
+                @Override
+                public void onSuccess(SendResult sendResult) {
+
+                }
+
+                @Override
+                public void onException(Throwable throwable) {
+
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
