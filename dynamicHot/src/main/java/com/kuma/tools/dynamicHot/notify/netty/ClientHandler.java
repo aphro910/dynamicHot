3e6b951c28fb1951bc3f0dca53090d1eead18dce
aphro_kuma <751923779@qq.com>
2025-07-21 17:40:58 +0800

    fix

--- dynamicHot/src/main/java/com/kuma/tools/dynamicHot/notify/netty/ClientHandler.java
+++ dynamicHot/src/main/java/com/kuma/tools/dynamicHot/notify/netty/ClientHandler.java
@@ -10,7 +10,6 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import io.netty.handler.timeout.IdleStateEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
